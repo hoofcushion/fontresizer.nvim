54b9e0bd933a60b4436cb185be614b9fb6aaac58
abcdefg233 <32760059+abcdefg233@users.noreply.github.com>
2023-12-06 22:22:26 +0800

    Use hcutil.nvim

--- lua/fontresizer/config.lua
+++ lua/fontresizer/config.lua
@@ -1,3 +1,4 @@
+local Util=require("hcutil")
 local M={}
 M.options={
  default_size=10,
@@ -19,23 +20,17 @@ local function integer(arg)
 end
 M.setup=function(user_options)
  local opts=vim.tbl_deep_extend("force",M.options,user_options or {})
- vim.validate({
-  Cursor      ={opts.default_size,"string"},
-  default_size={opts.default_size,function(arg) return integer(arg) and arg>0 end,"integer arg, 0<arg"},
-  change_up   ={opts.change_up,"number"},
-  change_down ={opts.change_down,"number"},
-  maximum     ={opts.maximum,function(arg) return number(arg) and arg>opts.minimum end,"integer arg, arg>minimum"},
-  minimum     ={opts.minimum,function(arg) return number(arg) and arg<opts.maximum end,"integer arg, arg<maximum"},
-  others      ={opts.others,function(arg)
-   if type(arg)=="table" then
-    vim.validate({
-     create_cmd={arg.create_cmd,"boolean"},
-     create_var={arg.create_var,"boolean"},
-     create_api={arg.create_api,"boolean"},
-    })
-    return true
-   end
-  end,"{create_cmd=boolean,create_var=boolean,create_api=boolean}"},
+ Util.validate_tab(opts,{
+  default_size={function(arg) return integer(arg) and arg>0 end,"integer arg, 0<arg"},
+  change_up   ="number",
+  change_down ="number",
+  maximum     ={function(arg) return number(arg) and arg>opts.minimum end,"integer arg, arg>minimum"},
+  minimum     ={function(arg) return number(arg) and arg<opts.maximum end,"integer arg, arg<maximum"},
+  others      ={
+   create_cmd="boolean",
+   create_var="boolean",
+   create_api="boolean",
+  },
  })
  M.options=opts
 end
